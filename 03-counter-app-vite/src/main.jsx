7daa60a9675b113e8af8ca0cae9feb68c3f97731
JasubiPL <gameportal7@hotmail.com>
2023-12-21 18:26:35 -0600

    primer componente

--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -1,12 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-function App(){
-  return (<h1>Hola Mundo !!!</h1>);
-}
+import { HelloWordApp } from "./HelloWordApp";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <HelloWordApp />
   </React.StrictMode>
 )
