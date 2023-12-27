faf7908956ea9765f594b69739581a244b203da3
JasubiPL <gameportal7@hotmail.com>
2023-12-27 09:29:17 -0600

    llamada a funciones he imprecion de retornos de funcion

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -1,5 +1,17 @@
+const getResult = () => 'hola mundo'
+
+const newMessage = {
+  message: 'Hola Mundo',
+  title: 'Jasubi'
+}
+
 export const FirstApp = () =>{
+
   return(
-    <h1>First App</h1>
+    <>
+      <h1>{ getResult() }</h1>
+      {/* <code>{ JSON.stringify( newMessage )}<code/> */}
+      <p>Soy un parrafo</p>
+    </>
   );
 }
