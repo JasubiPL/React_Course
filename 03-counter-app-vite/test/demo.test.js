972a7c55695f49fad55846e4d521add6b795be88
JasubiPL <gameportal7@hotmail.com>
2024-01-03 16:12:24 -0600

    Mi primer test

--- 03-counter-app-vite/test/demo.test.js
+++ 03-counter-app-vite/test/demo.test.js
@@ -1,5 +1,16 @@
-test('Esta prueba no debe de fallar', () => {
-  if (1 === 0){
-    throw new Error("No puedes dividir entre 0")
-  }
+describe('Pruebas en <DemoComponent />', () => {
+  
+  test('Esta prueba no debe de fallar', () => {
+
+    //1. Inicializacion
+    const message1 = 'Hola Mundo'
+  
+    //2. Estimulo
+    const message2 = message1.trim()
+  
+    //3. Observar el comportamiento... esperado
+    expect(message1).toBe(message2)
+  
+  })
+
 })
