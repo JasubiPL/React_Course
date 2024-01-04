27d7ab7a3b6c40b59bca7e8640a20bdb1913045d
JasubiPL <gameportal7@hotmail.com>
2024-01-03 18:09:11 -0600

    creando añadiendo elementos a un array con useState

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -1,7 +1,23 @@
+import { useState } from "react"
+
 export const GifExpertApp = () => {
+
+  const [category, setCategory] = useState(['One Punch', 'Dragon Ball'])
+
+  const onAddCategories = () =>{
+    setCategory([ 'Valorant', ...category ])
+  }
+
   return(
     <>
       <h1>Gif Expert App</h1>
+      <button onClick={ onAddCategories }> Agregar </button>
+
+      <ol>
+        { category.map(category => {
+          return <li key={ category }>{ category }</li>
+        }) }
+      </ol>
     </>
   )
 }
