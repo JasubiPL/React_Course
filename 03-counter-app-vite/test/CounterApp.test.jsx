3758bf3c3a056e25a5e7921889f3f9160ba26eed
JasubiPL <gameportal7@hotmail.com>
2024-01-08 08:49:28 -0600

    Se termino el testing del Counter

--- 03-counter-app-vite/test/CounterApp.test.jsx
+++ 03-counter-app-vite/test/CounterApp.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { CounterApp } from "../src/CounterApp";
 
 describe('Probando el componente CounterApp', () => {
@@ -13,5 +13,19 @@ describe('Probando el componente CounterApp', () => {
   test('Debe mostrar un valor inicial de 100', () => {
     render ( <CounterApp value={value}/> )
     expect(parseInt(screen.getByText(value).innerHTML)).toBe(100)
+  });
+
+  test('Debe aumentar el valor con el boton +1', () => {
+    render ( <CounterApp value={value}/> )
+    fireEvent.click(screen.getByText('+1'))
+    expect(screen.getByText('101')).toBeTruthy()
+    // screen.debug()
+  });
+
+  test('Debe reducir el valor con el boton -1', () => {
+    render ( <CounterApp value={value}/> )
+    fireEvent.click(screen.getByText('-1'))
+    expect(screen.getByText('99')).toBeTruthy()
+    screen.debug()
   });
 });
