cc29cf33c249630890da11793524ba1cb70e3d21
JasubiPL <gameportal7@hotmail.com>
2024-01-09 16:12:12 -0600

    UseEffect

---
+++ 05-hooks-app/src/02-useEffect/SimpleForm.jsx
@@ -0,0 +1,58 @@
+import { useEffect, useState } from "react"
+
+export const SimpleForm = () => {
+
+  const [formState, setFormState] = useState({
+    username: 'Jasubi',
+    email: 'jasubi@gmail.com'
+  })
+
+  const { username, email } = formState
+
+  const onInputChange = ({ target }) =>{
+    const { name, value } = target
+    setFormState({
+      ...formState,
+      [ name ]:value
+    })
+  }
+
+
+  useEffect(() =>{
+    console.log('useEffect Called')
+  }, [])
+
+  useEffect(() =>{
+    console.log('Form Change')
+  }, [formState])
+
+  useEffect(() =>{
+    console.log('Email change')
+  }, [email])
+
+
+  return(
+    <>
+      <h1>Formulario Simple</h1>
+      <hr />
+
+      <input 
+        type="text"
+        className="form-control"
+        placeholder="Username"
+        name="username"
+        value={ username }
+        onChange={ (e) => onInputChange(e) }
+      />
+
+      <input 
+        type="email"
+        className="form-control mt-2"
+        placeholder="user@email.com"
+        name="email"
+        value={ email }
+        onChange={ (e) => onInputChange(e)}
+      />
+    </>
+  )
+}
