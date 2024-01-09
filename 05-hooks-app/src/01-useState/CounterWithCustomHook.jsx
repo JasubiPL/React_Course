a39814eec2fd4f6d6124572673c39c54a39dffac
JasubiPL <gameportal7@hotmail.com>
2024-01-09 15:39:29 -0600

    UseCounter

---
+++ 05-hooks-app/src/01-useState/CounterWithCustomHook.jsx
@@ -0,0 +1,18 @@
+import { useCounter } from "../../hooks/useCounter"
+
+export const CounterWithCustomHook = () =>{
+
+  const { counter, increment, decrement, reset } = useCounter();
+
+  return(
+    <>
+      <h1>Counter With CustomHook: { counter }</h1>
+
+      <hr />
+
+      <button className="btn btn-danger" onClick={ () => decrement() }> -1 </button>
+      <button className="btn btn-warning" onClick={ reset }> Reset </button>
+      <button className="btn btn-success" onClick={ () => increment() }> +1 </button>
+    </>
+  )
+}
